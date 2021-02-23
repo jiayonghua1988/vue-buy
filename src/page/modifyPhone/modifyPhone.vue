4a2c553d5b2215a56263c67261b3b1a7a96d4efc
jiayonghua-android <divehome@163.com>
2021-02-23 11:02:23 +0800

    修改手机号

--- src/page/modifyPhone/modifyPhone.vue
+++ src/page/modifyPhone/modifyPhone.vue
@@ -13,7 +13,7 @@
             <span class="label">验证码：</span>
             <input type="text" placeholder=" 请输入" v-model="msgCode" class="input-content" maxlength="6" @input="validateCode">
         </section>
-        <section class="send-sms">发送验证码</section>
+        <section :class="{'send-sms' : btnLabelEnable,'send-sms-disenable' : !btnLabelEnable}" @click="sendMsg">{{btnLabel}}</section>
       </section>
     </section>
     <save-button label="保存" @btnClick='save'></save-button>
@@ -22,12 +22,17 @@
 
 <script>
 import SaveButton from '../../components/save-button'
+
+import { sendPhone } from '../../assets/requestUrl'
 export default {
   data () {
     return {
       originalPhone: '13333333333',
       newPhone: '',
-      msgCode: ''
+      msgCode: '',
+      timeInterval: '',
+      btnLabel: '发送验证码',
+      btnLabelEnable: true
     }
   },
   components: {
@@ -42,6 +47,29 @@ export default {
     },
     validateCode () {
       this.msgCode = this.msgCode.replace(/\D/g, '')
+    },
+    sendMsg () {
+      if (this.newPhone === '') {
+        this.$toast('新手机号不能为空')
+      } else {
+        if (!this.btnLabelEnable) {
+          return
+        }
+        sendPhone(this.newPhone, res => {
+          this.$toast('验证码已发送')
+          let timeLong = 60
+          this.btnLabelEnable = false
+          this.timeInterval = setInterval(() => {
+            timeLong -= 1
+            this.btnLabel = timeLong
+            if (timeLong === 0) {
+              this.btnLabelEnable = true
+              clearInterval(this.timeInterval)
+              this.timeInterval = null
+            }
+          }, 1000)
+        })
+      }
     }
   }
 }
@@ -113,6 +141,19 @@ export default {
   margin: 30px 30px 0px 30px;
   padding-left: 15px;
   padding-right: 15px;
+  border-radius: 10px;
+}
+.send-sms-disenable {
+  background-color: #D0CCCC;
+  width: 272px;
+  height: 90px;
+  color: #fff;
+  font-size: 28px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 30px 0px 0px;
   border-radius: 10px;
 }
 </style>
